63821ec5823a789f89326fb6e079eab7db19a067
AnissDa <aniss.dahaoui@hotmail.com>
2026-04-06 05:34:51 +0200

    ajout du code pour l'exercice horoscope (js basique mini projet)

---
+++ Horoscope.js
@@ -0,0 +1,311 @@
+
+
+console.log("Month of your birth?");
+let userMonth=12;
+let nbQuote=Math.floor(Math.random()*5);
+let quote="";
+// boucle pour choisir le message a ecrire en fonction du mois
+if(nbQuote ===0)
+{
+    quote="imothep";
+}
+else if(nbQuote===1)
+{
+    quote="tout va bien"
+}
+else if(nbQuote===2)
+{
+    quote="tout va mal"
+}
+else if(nbQuote===3)
+{
+    quote="tout va bienooooo"
+}
+else
+{
+    quote="c'est la merde"
+}
+
+// annonce du moi de naissance
+if (userMonth == 1) {
+    console.log("Mon mois de naissance est janvier");
+} else if (userMonth == 2) {
+    console.log("Mon mois de naissance est février");
+} else if (userMonth == 3) {
+    console.log("Mon mois de naissance est mars");
+} else if (userMonth == 4) {
+    console.log("Mon mois de naissance est avril");
+} else if (userMonth == 5) {
+    console.log("Mon mois de naissance est mai");
+} else if (userMonth == 6) {
+    console.log("Mon mois de naissance est juin");
+} else if (userMonth == 7) {
+    console.log("Mon mois de naissance est juillet");
+} else if (userMonth == 8) {
+    console.log("Mon mois de naissance est août");
+} else if (userMonth == 9) {
+    console.log("Mon mois de naissance est septembre");
+} else if (userMonth == 10) {
+    console.log("Mon mois de naissance est octobre");
+} else if (userMonth == 11) {
+    console.log("Mon mois de naissance est novembre");
+} else if (userMonth == 12) {
+    console.log("Mon mois de naissance est décembre");
+} else {
+    console.log("Mois invalide");
+}
+
+if(userMonth ==1 && nbQuote == 0){
+    console.log(quote);
+
+}
+else if (userMonth==1 && nbQuote==1)
+{
+    console.log(quote);
+}
+else if (userMonth==1 && nbQuote==2)
+{
+    console.log(quote);
+}
+else if (userMonth==1 && nbQuote==3)
+{
+    console.log(quote);
+}
+else if (userMonth==1 && nbQuote==4)
+{
+    console.log(quote);
+}
+if(userMonth ==2 && nbQuote == 0){
+    console.log(quote);
+
+}
+else if (userMonth==2 && nbQuote==1)
+{
+    console.log(quote);
+}
+else if (userMonth==2 && nbQuote==2)
+{
+    console.log(quote);
+}
+else if (userMonth==2 && nbQuote==3)
+{
+    console.log(quote);
+}
+else if (userMonth==2 && nbQuote==4)
+{
+    console.log(quote);
+}//3
+
+if(userMonth ==3 && nbQuote == 0){
+    console.log(quote);
+
+}
+else if (userMonth==3 && nbQuote==1)
+{
+    console.log(quote);
+}
+else if (userMonth==3 && nbQuote==2)
+{
+    console.log(quote);
+}
+else if (userMonth==3 && nbQuote==3)
+{
+    console.log(quote);
+}
+else if (userMonth==3 && nbQuote==4)
+{
+    console.log(quote);
+}
+//4
+if(userMonth ==4 && nbQuote == 0){
+    console.log(quote);
+
+}
+else if (userMonth==4 && nbQuote==1)
+{
+    console.log(quote);
+}
+else if (userMonth==4 && nbQuote==2)
+{
+    console.log(quote);
+}
+else if (userMonth==4 && nbQuote==3)
+{
+    console.log(quote);
+}
+else if (userMonth==4 && nbQuote==4)
+{
+    console.log(quote);
+}
+
+//5
+if(userMonth ==5 && nbQuote == 0){
+    console.log(quote);
+
+}
+else if (userMonth==5 && nbQuote==1)
+{
+    console.log(quote);
+}
+else if (userMonth==5 && nbQuote==2)
+{
+    console.log(quote);
+}
+else if (userMonth==5 && nbQuote==3)
+{
+    console.log(quote);
+}
+else if (userMonth==5 && nbQuote==4)
+{
+    console.log(quote);
+}
+//6
+if(userMonth ==6 && nbQuote == 0){
+    console.log(quote);
+
+}
+else if (userMonth==6 && nbQuote==1)
+{
+    console.log(quote);
+}
+else if (userMonth==6 && nbQuote==2)
+{
+    console.log(quote);
+}
+else if (userMonth==6 && nbQuote==3)
+{
+    console.log(quote);
+}
+else if (userMonth==6 && nbQuote==4)
+{
+    console.log(quote);
+}
+//7
+if(userMonth ==7 && nbQuote == 0){
+    console.log(quote);
+
+}
+else if (userMonth==7 && nbQuote==1)
+{
+    console.log(quote);
+}
+else if (userMonth==7 && nbQuote==2)
+{
+    console.log(quote);
+}
+else if (userMonth==7 && nbQuote==3)
+{
+    console.log(quote);
+}
+else if (userMonth==7 && nbQuote==4)
+{
+    console.log(quote);
+}
+//8
+if(userMonth ==8 && nbQuote == 0){
+    console.log(quote);
+
+}
+else if (userMonth==8 && nbQuote==1)
+{
+    console.log(quote);
+}
+else if (userMonth==8 && nbQuote==2)
+{
+    console.log(quote);
+}
+else if (userMonth==8 && nbQuote==3)
+{
+    console.log(quote);
+}
+else if (userMonth==8 && nbQuote==4)
+{
+    console.log(quote);
+}
+//9
+if(userMonth ==9 && nbQuote == 0){
+    console.log(quote);
+
+}
+else if (userMonth==9 && nbQuote==1)
+{
+    console.log(quote);
+}
+else if (userMonth==9 && nbQuote==2)
+{
+    console.log(quote);
+}
+else if (userMonth==9 && nbQuote==3)
+{
+    console.log(quote);
+}
+else if (userMonth==9 && nbQuote==4)
+{
+    console.log(quote);
+}
+//10
+if(userMonth ==10 && nbQuote == 0){
+    console.log(quote);
+
+}
+else if (userMonth==10 && nbQuote==1)
+{
+    console.log(quote);
+}
+else if (userMonth==10 && nbQuote==2)
+{
+    console.log(quote);
+}
+else if (userMonth==10 && nbQuote==3)
+{
+    console.log(quote);
+}
+else if (userMonth==10 && nbQuote==4)
+{
+    console.log(quote);
+}
+//11
+if(userMonth ==11 && nbQuote == 0){
+    console.log(quote);
+
+}
+else if (userMonth==11 && nbQuote==1)
+{
+    console.log(quote);
+}
+else if (userMonth==11 && nbQuote==2)
+{
+    console.log(quote);
+}
+else if (userMonth==11 && nbQuote==3)
+{
+    console.log(quote);
+}
+else if (userMonth==11 && nbQuote==4)
+{
+    console.log(quote);
+}
+//12
+if(userMonth ==12 && nbQuote == 0){
+    console.log(quote);
+
+}
+else if (userMonth==12 && nbQuote==1)
+{
+    console.log(quote);
+}
+else if (userMonth==12 && nbQuote==2)
+{
+    console.log(quote);
+}
+else if (userMonth==12 && nbQuote==3)
+{
+    console.log(quote);
+}
+else if (userMonth==12 && nbQuote==4)
+{
+    console.log(quote);
+}
+
+
+
